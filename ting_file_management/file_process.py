a05816ec297c9b06e95de214ae0f5eb7f13b344f
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2023-05-08 20:12:53 -0300

    feat: implementa funcao process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    lines = txt_importer(path_file)
+    file = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines,
+    }
+
+    for i in range(0, len(instance)):
+        if instance.search(i)["nome_do_arquivo"] == path_file:
+            return None
+
+    instance.enqueue(file)
+    print(file, file=sys.stdout)
 
 
 def remove(instance):
