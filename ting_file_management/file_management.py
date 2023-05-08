945b817252daee6c049c0ce5333d0a1b56314f7d
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2023-05-08 19:39:19 -0300

    feat: implementa a funcao txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+    try:
+        with open(path_file, "r") as file:
+            lines = [line.strip() for line in file]
+    except OSError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+    else:
+        return lines
